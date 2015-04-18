64f926f4b1c711b18c971125864bdd190cd0ce16
Alvaro <Alvaro@apinar>
2015-04-19 00:38:22 +0200

    Implementado fromWasteToFundation. Tests OK.

--- klondike/src/klondike/MoveCardController.java
+++ klondike/src/klondike/MoveCardController.java
@@ -1,5 +1,6 @@
 package klondike;
 
+import java.util.ArrayList;
 import java.util.Stack;
 
 import klondike.Card;
@@ -18,20 +19,51 @@ public class MoveCardController {
 		if (game.getDeck().size() == 0){
 			throw new RuntimeException ( "Forbidden" ) ;
 		} else{
-			Card card = game.getDeck().pop();
+			Stack<Card> deck = game.getDeck();
+			Card card = deck.pop();
 			card.turnOver();
 			Stack<Card> stack = game.getWaste();
 			stack.push(card);
 			game.setWaste(stack);
+			game.setDeck(deck);
 		}
 				
 	}
 
-	public void fromWasteToFoundation(){
-		
+	public void fromWasteToFoundation(Integer index) throws Exception{
+		Card card = game.getWaste().peek();
+		if (card.getFace()==1 ){
+			if (game.getFoundations().get(index).size() != 0){
+				throw new RuntimeException ( "Forbidden" ) ;
+			} else{				
+				makeMoveWasteToFoundation(index);
+			}
+			
+		} else {
+			if (game.getFoundations().get(index).size() ==0)
+				throw new RuntimeException ( "Forbidden" ) ;
+			else
+				if ( card.getFace()-game.getFoundations().get(index).peek().getFace() != 1){
+					throw new RuntimeException ( "Forbidden" ) ;
+				}else{
+					if ( card.getSuit() == game.getFoundations().get(index).peek().getSuit())
+						makeMoveWasteToFoundation(index);
+					}
+				}
 	}
-	
-	
+			
+
+	private void makeMoveWasteToFoundation(Integer index) {
+		Stack<Card> waste = game.getWaste();
+		Card card = waste.pop();
+		Stack<Card> foundation = game.getFoundations().get(index);
+		ArrayList<Stack<Card>> foundations = game.getFoundations();
+		foundation.push(card);
+		foundations.set(index, foundation);
+		game.setFoundations(foundations);				
+	}
+
+
 	public void fromWasteToTableau(){
 		
 	}
--- klondike/src/test/MoveCardControllerTest.java
+++ klondike/src/test/MoveCardControllerTest.java
@@ -2,6 +2,7 @@ package test;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
 import java.util.Stack;
 
 import klondike.Card;
@@ -88,16 +89,85 @@ public class MoveCardControllerTest {
 		
 	
 	@Test
-	public void fromWasteFirstToFoundationTest(){
+	public void fromWasteAceToFoundationTest() throws Exception{
+		Game game = moveCardController.getGame();
+		Stack<Card> stack = game.getWaste();
+		stack.add(new Card(1,Suits.DIAMONDS));
+		game.setWaste(stack);	
+		moveCardController.fromWasteToFoundation(0);
+		//movemos un as a un palo vacío
+		assertEquals(Integer.valueOf(1) , moveCardController.getGame().getFoundations().get(0).peek().getFace());
 		
-		
+		ArrayList<Stack<Card>> foundations = game.getFoundations();
+		Stack<Card> foundation = foundations.get(1);
+		foundation.add(new Card(6,Suits.CLUBS));
+		foundations.set(1, foundation);
+		game.setFoundations(foundations);
+		stack = game.getWaste();
+		stack.add(new Card(1,Suits.DIAMONDS));
+		game.setWaste(stack);	
+				
+		try {
+			moveCardController.fromWasteToFoundation(1);
+			//movemos un as a un palo que no le corresponde
+		    fail("Movimiento no permitido");
+		  } catch (Exception e) {
+			  assertEquals(e.getMessage(), "Forbidden");
+		  }	
 	}
 	
 	
 	@Test
-	public void fromWasteNotFirstToFoundationTest(){
+	public void fromWasteNotAceToFoundationTest() throws Exception{
+		Game game = moveCardController.getGame();
+		Stack<Card> stack = game.getWaste();
+		stack.add(new Card(5,Suits.DIAMONDS));
+		game.setWaste(stack);
+		//moveCardController.fromWasteToFoundation(1);
+		
+		//Movemos a un palo correcto
+		ArrayList<Stack<Card>> foundations = game.getFoundations();
+		Stack<Card> foundation = foundations.get(1);
+		foundation.push(new Card(4,Suits.DIAMONDS));
+		foundations.set(1, foundation);
+		game.setFoundations(foundations);
+		moveCardController.fromWasteToFoundation(1);
+		assertTrue(moveCardController.getGame().getFoundations().get(1).peek().getFace()==5);
+		assertTrue(moveCardController.getGame().getFoundations().get(1).peek().getSuit()==Suits.DIAMONDS);
+			
+		
+		//Movemos a un palo con un nº no consecutivo
+		stack = game.getWaste();
+		stack.add(new Card(6,Suits.DIAMONDS));
+		game.setWaste(stack);
+		foundations = game.getFoundations();
+		foundation = foundations.get(2);
+		foundation.add(new Card(4,Suits.DIAMONDS));
+		foundations.set(2, foundation);
+		game.setFoundations(foundations);
+		try {
+			moveCardController.fromWasteToFoundation(2);
+			 fail("Movimiento no permitido");
+		  } catch (Exception e) {
+			  assertEquals(e.getMessage(), "Forbidden");
+		  }	
 		
 		
+		//Movemos a un palo que no corresponde con el palo de la carta a mover
+		stack = game.getWaste();
+		stack.add(new Card(7,Suits.DIAMONDS));
+		game.setWaste(stack);
+		foundations = game.getFoundations();
+		foundation = foundations.get(3);
+		foundation.add(new Card(8,Suits.HEARTS));
+		foundations.set(3, foundation);
+		game.setFoundations(foundations);
+		try {
+			moveCardController.fromWasteToFoundation(3);
+			 fail("Movimiento no permitido");
+		  } catch (Exception e) {
+			  assertEquals(e.getMessage(), "Forbidden");
+		  }					
 	}
 	
 	
